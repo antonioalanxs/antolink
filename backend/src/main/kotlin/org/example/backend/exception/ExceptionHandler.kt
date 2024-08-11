1370c5510f9561041e634d8f4a3504d59a9f84ec
Antonio José Alanís Bernal <antoniojalanis7131@gmail.com>
2024-08-11 17:18:56 +0200

    fix: Fix JSON serialization error

--- backend/src/main/kotlin/org/example/backend/exception/ExceptionHandler.kt
+++ backend/src/main/kotlin/org/example/backend/exception/ExceptionHandler.kt
@@ -2,6 +2,7 @@ package org.example.backend.exception
 
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -28,9 +29,9 @@ class ExceptionHandler {
         return ResponseEntity(body, HttpStatus.BAD_REQUEST)
     }
 
-    @ExceptionHandler(IllegalArgumentException::class)
+    @ExceptionHandler(IllegalArgumentException::class, HttpMessageNotReadableException::class)
     fun handleIllegalArgumentException(
-        exception: IllegalArgumentException
+        exception: Exception
     ): ResponseEntity<Response> {
         val body = Response(
             HttpStatus.BAD_REQUEST,
